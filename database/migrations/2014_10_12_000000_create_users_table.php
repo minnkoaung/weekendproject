33d02db70032fff13b2a77d312a84980c276b6eb
heinhtut <thanhtutoo95@gmail.com>
2017-06-10 16:42:27 +0630

    change education to nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
 
-            $table->string("education");
+            $table->string("education")->nullable()->default('NULL');
 
             
             $table->boolean("is_super")->default(false);
