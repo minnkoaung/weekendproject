50bb81ac8db89d7672c4ae7460b27f843c24b0db
minnkoaung <minnkoaung@gmail.com>
2017-06-10 12:54:29 +0630

    migration&seeding added

--- database/migrations/2017_06_03_122131_create_roles_table.php
+++ database/migrations/2017_06_03_122131_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean("is_admin")->default(false);
-            $table->boolean("is_super")->default(false);
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->jsonb('permissions'); // jsonb deletes duplicates
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 }
---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        
+        // User::create([
+        // 	'name'	=> 'Super Admin',
+        // 	'email'	=> 'admin@wpa26.org',
+        // 	'password'	=> bcrypt("123456"),
+        // 	'is_admin'	=> true,
+        // 	'is_super'	=> true,
+        // 	]);
+
+        User::create([
+            'name'  => 'Admin',
+            'email' => 'administrator@wpa26.org',
+            'password'  => bcrypt("123456"),
+            'is_admin'  => true,
+            'is_super'  => false
+            ]);
+
+    }
+}
