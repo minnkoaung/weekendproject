219fcd04ce5a78970f99ae0e17c737ec84bdc924
heinhtut <thanhtutoo95@gmail.com>
2017-06-10 16:38:53 +0630

    added datable

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -4,6 +4,7 @@
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
 
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet" type="text/css" />
 
